b9f8ce839d2561cecff737dda04d15ee2470d1b3
Ommesalma25 <86910484+Ommesalma25@users.noreply.github.com>
2021-07-07 11:10:11 +0530

    php quiz

--- admin.php
+++ admin.php
@@ -1,98 +1,58 @@
 <?php
-    include_once 'database.php';
-    session_start();
-    if(isset($_SESSION["email"]))
-	{
-		session_destroy();
-    }
-    
-    $ref=@$_GET['q'];
-    if(isset($_POST['submit']))
-	{	
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+session_start();
+include "connection.php";
+if (isset($_SESSION['admin'])) {
+	header("location: adminhome.php");
+}
+if (isset($_POST['password']))  {
+	$password = mysqli_real_escape_string($conn , $_POST['password']);
+	$adminpass = '$2y$10$8WkSLFcoaqhJUJoqjg3K8eWixJWswsICf7FTxehKmx8hpmIKYWqju';
+	if (password_verify($password , $adminpass)) {
+		$_SESSION['admin'] = "active";
+		header("Location: adminhome.php");
+	}
+	else {
+		echo  "<script> alert('wrong password');</script>";
+	}
+}
 
-        $email = stripslashes($email);
-        $email = addslashes($email);
-        $password = stripslashes($password); 
-        $password = addslashes($password);
 
-        $email = mysqli_real_escape_string($con,$email);
-        $password = mysqli_real_escape_string($con,$password);
-        
-        $result = mysqli_query($con,"SELECT email FROM admin WHERE email = '$email' and password = '$password'") or die('Error');
-        $count=mysqli_num_rows($result);
-        if($count==1)
-        {
-            session_start();
-            if(isset($_SESSION['email']))
-            {
-                session_unset();
-            }
-            $_SESSION["name"] = 'Admin';
-            $_SESSION["key"] ='admin';
-            $_SESSION["email"] = $email;
-            header("location:dashboard.php?q=0");
-        }
-        else
-        {
-            echo "<center><h3><script>alert('Sorry.. Wrong Username (or) Password');</script></h3></center>";
-            header("refresh:0;url=admin.php");
-        }
-    }
 ?>
-<!DOCTYPE html>
+
+
+
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Admin Login | Online Quiz System</title>
-		<link rel="stylesheet" href="scripts/bootstrap/bootstrap.min.css">
-		<link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
-		<link rel="stylesheet" href="css/form.css">
-        <style type="text/css">
-            body{
-                  width: 100%;
-                  background: url(image/book1.jpg) ;
-                  background-position: center center;
-                  background-repeat: no-repeat;
-                  background-attachment: fixed;
-                  background-size: cover;
-                }
-          </style>
+		<title>PHP-kuiz</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 
 	<body>
-		<section class="login first grey">
+		<header>
 			<div class="container">
-				<div class="box-wrapper">				
-					<div class="box box-border">
-						<div class="box-body">
-						<center> <h5 style="font-family: Noto Sans;">Login to </h5><h4 style="font-family: Noto Sans;">Admin Page</h4></center><br>
-							<form method="post" action="admin.php" enctype="multipart/form-data">
-								<div class="form-group">
-									<label>Enter Your Email Id:</label>
-									<input type="email" name="email" class="form-control">
-								</div>
-								<div class="form-group">
-									<label class="fw">Enter Your Password:
-										<a href="javascript:void(0)" class="pull-right">Forgot Password?</a>
-									</label>
-									<input type="password" name="password" class="form-control">
-								</div> 
-								<div class="form-group text-right">
-									<button class="btn btn-primary btn-block" name="submit">Login</button>
-								</div>
-								
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+				<h1>PHP-kuiz</h1>
+				<a href="index.php" class="start">Home</a>
 
-		<script src="js/jquery.js"></script>
-		<script src="scripts/bootstrap/bootstrap.min.js"></script>
+			</div>
+		</header>
+
+		<main>
+		<div class="container">
+				<h2>Enter Password</h2>
+				<form method="POST" action="">
+				<input type="password" name="password" required="" >
+				<input type="submit" name="submit" value="send"> 
+
+			</div>
+
+
+		</main>
+
+		<footer>
+			<div class="container">
+				Copyright @ PHP-kuiz
+			</div>
+		</footer>
+
 	</body>
 </html>
--- index.php
+++ index.php
@@ -1,51 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>| Online Quiz System |</title>
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
-        <style type="text/css">
-            body 
-            {
-                width: 100%;
-                background: url(image/book1.jpg) ;
-                background-position: center center;
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-size: cover;
-            }
-            h3
-            {
-                color: #fff;
-                text-transform: uppercase;
-                font-size: 70px;
-                font-weight: 600;
-                letter-spacing: 0.015em;
-            }
-            h3::after 
-            {
-                content: '';
-                width: 200px;
-                display: block;
-                background: #ffee58;
-                height: 6px;
-                margin: 30px auto;
-                line-height: 1.1;
-            }
-        </style>
-    </head>
-    <body>
-        <div style="padding-right: 270px; padding-top: 100px;"> <a href="admin.php" class="btn" style="float: right;"> admin login </a></div>
-        <center>
-            <div class="intro">
-                <h3> online quiz system </h3>
-                <a href="login.php" class="btn"> login </a> &emsp;
-                <a href="register.php" class="btn"> register </a>
-                <h2> Good &nbsp;Luck. </h2>
-            </div>
-        </center>
-    </body>
+<?php
+session_start();
+include "connection.php";
+?>
+<?php 
+if (isset($_SESSION['id'])) {
+	header("location: home.php");
+}
+?>
+<?php
+if (isset($_POST['email'])) {
+$email = mysqli_real_escape_string($conn , $_POST['email']);
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	$checkmail = "SELECT * from users WHERE email = '$email'";
+	$runcheck = mysqli_query($conn , $checkmail) or die(mysqli_error($conn));
+	if (mysqli_num_rows($runcheck) > 0) {
+		$played_on = date('Y-m-d H:i:s');
+		$update = "UPDATE users SET played_on = '$played_on' WHERE email = '$email' ";
+		$runupdate = mysqli_query($conn , $update) or die(mysqli_error($conn));
+		$row = mysqli_fetch_array($runcheck);
+			$id = $row['id'];
+			$_SESSION['id'] = $id;
+			$_SESSION['email'] = $row['email'];
+		header("location: home.php");
+	}
+	else {
+		$played_on = date('Y-m-d H:i:s');
+	$query = "INSERT INTO users(email,played_on) VALUES ('$email','$played_on')";
+	$run = mysqli_query($conn, $query) or die(mysqli_error($conn)) ;
+	if (mysqli_affected_rows($conn) > 0) {
+		$query2 = "SELECT * FROM users WHERE email = '$email' ";
+		$run2 = mysqli_query($conn , $query2) or die(mysqli_error($conn));
+		if (mysqli_num_rows($run2) > 0) {
+			$row = mysqli_fetch_array($run2);
+			$id = $row['id'];
+			$_SESSION['id'] = $id;
+			$_SESSION['email'] = $row['email'];
+			header("location: home.php");
+		}
+}
+	else {
+		echo "<script> alert('something is wrong'); </script>";
+	}
+}
+}
+else {
+	echo "<script> alert('Invalid Email'); </script>";
+}
+}
+
+
+
+?>
+<html>
+	<head>
+		<title>PHP-kuiz</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+
+	<body>
+		<header>
+			<div class="container">
+				<h1>PHP-kuiz</h1>
+				<a href="index.php" class="start">Home</a>
+				<a href="admin.php" class="start">Admin Panel</a>
+
+			</div>
+		</header>
+
+		<main>
+		<div class="container">
+				<h2>Enter Your Email</h2>
+				<form method="POST" action="">
+				<input type="email" name="email" required="" >
+				<input type="submit" name="submit" value="PLAY NOW">
+
+			</div>
+
+
+		</main>
+
+		<footer>
+			<div class="container">
+				Copyright @ PHP-kuiz
+			</div>
+		</footer>
+
+	</body>
 </html>
